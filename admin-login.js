f84d41b1e86e54e2072f2b7b450557cd24d69811
hackpasantes-dev <hackpasantes@gmail.com>
2026-02-16 15:20:52 -0400

    Update admin-login.js

--- admin-login.js
+++ admin-login.js
@@ -2,10 +2,11 @@ document.getElementById("login").addEventListener("click", () => {
   const user = document.getElementById("user").value;
   const pass = document.getElementById("pass").value;
 
-  if (user === "admin" && pass === "1234") {
+  if (user === "admin" && pass === "12345") {
     localStorage.setItem("admin", "true");
     window.location.href = "admin-panel.html";
   } else {
     document.getElementById("msg").textContent = "❌ Credenciales incorrectas";
   }
 });
+
